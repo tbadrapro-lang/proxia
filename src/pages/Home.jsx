d41ce691e3047939720c70f087e5a11a9f508488
Badra <tbadra94@gmail.com>
2026-04-18 22:31:28 +0200

    Suppression doublons et sections inutiles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,9 @@ import Hero from '../sections/Hero';
 import LogoMarquee from '../components/LogoMarquee';
 import CommercesProximite from '../sections/CommercesProximite';
 import Services from '../sections/Services';
-import PourQui from '../sections/PourQui';
 import Realisations from '../sections/Realisations';
 import Avis from '../sections/Avis';
-import Process from '../sections/Process';
 import Tarifs from '../sections/Tarifs';
-import Temoignages from '../sections/Temoignages';
 import FAQ from '../sections/FAQ';
 import Contact from '../sections/Contact';
 import Footer from '../components/Footer';
@@ -24,14 +21,11 @@ export default function Home() {
       <main>
         <section id="accueil"><Hero /></section>
         <LogoMarquee />
-        <CommercesProximite />
         <section id="services"><Services /></section>
-        <section id="pour-qui"><PourQui /></section>
+        <section id="pour-qui"><CommercesProximite /></section>
         <section id="realisations"><Realisations /></section>
-        <Avis />
-        <section id="process"><Process /></section>
+        <section id="avis"><Avis /></section>
         <section id="tarifs"><Tarifs /></section>
-        <section id="temoignages"><Temoignages /></section>
         <section id="faq"><FAQ /></section>
         <section id="contact"><Contact /></section>
       </main>
